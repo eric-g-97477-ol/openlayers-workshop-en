4989403307932c58e8fba1e99cabb83858eff269
Eric G <eric.gorr@twosixtech.com>
2023-03-08 08:43:21 -0500

    [master] Vector Data-Drag and drop

--- main.js
+++ main.js
@@ -4,21 +4,31 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
 import Link from 'ol/interaction/Link';
+import DragAndDrop from 'ol/interaction/DragAndDrop';
 
 const map = new Map({
   target: 'map-container',
-  layers: [
-    new VectorLayer({
-      source: new VectorSource({
-        format: new GeoJSON(),
-        url: './data/countries.json',
-      }),
-    }),
-  ],
+
   view: new View({
     center: [0, 0],
     zoom: 2,
   }),
 });
+
+const source = new VectorSource();
+
+const layer = new VectorLayer({
+    source: source,
+  });
+
+map.addLayer(layer);
+
+map.addInteraction(
+    new DragAndDrop({
+      source: source,
+      formatConstructors: [GeoJSON],
+    })
+  );
+
 
 map.addInteraction(new Link());
